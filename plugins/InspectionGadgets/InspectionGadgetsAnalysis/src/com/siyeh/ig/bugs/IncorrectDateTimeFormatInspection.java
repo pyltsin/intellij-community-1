ffe3c6f70a9b96f7614ed180fd4d1ec079a6d631
pyltsin-m <pyltsin-m@yandex.ru>
2022-07-15 22:19:22 +0300

    new inspection

--- plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/IncorrectDateTimeFormatInspection.java
+++ plugins/InspectionGadgets/InspectionGadgetsAnalysis/src/com/siyeh/ig/bugs/IncorrectDateTimeFormatInspection.java
@@ -64,32 +64,37 @@ public class IncorrectDateTimeFormatInspection extends AbstractBaseJavaLocalInsp
 
   Map<Character, CountVerifier> DATE_TIME_FORMATTER_ALLOWED = Map.ofEntries(
     Map.entry('G', rangeOf(1, 5)),
-    Map.entry('u', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('y', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('Y', rangeOf(1, Integer.MAX_VALUE)),
+    Map.entry('u', rangeOf(1, 19)),
+    Map.entry('y', rangeOf(1, 19)),
+    Map.entry('Y', rangeOf(1, Integer.MAX_VALUE)), //according to refs must be until 19, but now there is no exception
     Map.entry('Q', rangeOf(1, 5)),
     Map.entry('q', rangeOf(1, 5)),
     Map.entry('M', rangeOf(1, 5)),
     Map.entry('L', rangeOf(1, 5)),
     Map.entry('w', rangeOf(1, 2)),
+    Map.entry('W', rangeOf(1, 1)),
     Map.entry('d', rangeOf(1, 2)),
     Map.entry('D', rangeOf(1, 3)),
     Map.entry('F', rangeOf(1, 1)),
-    Map.entry('g', rangeOf(1, Integer.MAX_VALUE)),
+    Map.entry('g', rangeOf(1, 19)),
     Map.entry('E', rangeOf(1, 5)),
     Map.entry('e', rangeOf(1, 5)),
     Map.entry('c', rangeOf(1, 5)),
+
     Map.entry('a', rangeOf(1, 1)),
     Map.entry('h', rangeOf(1, 2)),
     Map.entry('H', rangeOf(1, 2)),
     Map.entry('k', rangeOf(1, 2)),
     Map.entry('K', rangeOf(1, 2)),
     Map.entry('m', rangeOf(1, 2)),
-    Map.entry('s', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('S', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('A', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('n', rangeOf(1, Integer.MAX_VALUE)),
-    Map.entry('N', rangeOf(1, Integer.MAX_VALUE)),
+    Map.entry('s', rangeOf(1, 2)),
+    Map.entry('S', rangeOf(1, 9)), //limit as nanos
+    Map.entry('A', rangeOf(1, 19)),
+    Map.entry('n', rangeOf(1, 19)),
+    Map.entry('N', rangeOf(1, 19)),
+
+    Map.entry('B', setOf(1, 4, 5)),
+
     Map.entry('V', rangeOf(2, 2)),
     Map.entry('v', setOf(1, 4)),
     Map.entry('z', rangeOf(1, 4)),
@@ -122,31 +127,41 @@ public class IncorrectDateTimeFormatInspection extends AbstractBaseJavaLocalInsp
         if (!(patternObject instanceof String)) return;
         String pattern = (String)patternObject;
         List<Token> tokens = new ArrayList<>();
-        char lastChar = 0;
         int optionalDepth = 0;
         char[] array = pattern.toCharArray();
         boolean inQuote = false;
         int lastQuoteIndex = 0;
-        int length = 1;
+        int length = 0;
         for (int pos = 0; pos < array.length; pos++) {
           char c = array[pos];
+
+          //process tokens
           if (!inQuote && isLetter(c)) {
-            if (c == lastChar) {
-              length++;
+            length++;
+            if (pos != array.length - 1 && c == array[pos + 1]) {
               continue;
             }
-            tokens.add(new Token(pos, array, length));
-            checkPadding(expression, array, pos);
-
-            lastChar = c;
-            length = 1;
-            continue;
+            else {
+              tokens.add(new Token(pos - length + 1, array, length));
+              checkPadding(expression, array, pos - length + 1, pos + 1);
+            }
           }
+          length = 0;
+
+
+          //check if it starts quote region or one quote
+          //case with one quote ('') is processed as quote region
           if (c == '\'') {
             inQuote = !inQuote;
             lastQuoteIndex = pos;
+            continue;
           }
-          checkUnsupportedProblem(expression, pos, c);
+          //skip whole region in quote
+          if (inQuote) {
+            continue;
+          }
+
+          checkUnsupportedSymbolsProblem(expression, pos, c);
 
           if (c == '[') {
             optionalDepth++;
@@ -183,7 +198,7 @@ public class IncorrectDateTimeFormatInspection extends AbstractBaseJavaLocalInsp
         }
       }
 
-      private void checkUnsupportedProblem(@NotNull PsiExpression expression, int pos, char c) {
+      private void checkUnsupportedSymbolsProblem(@NotNull PsiExpression expression, int pos, char c) {
         if (c == '{' || c == '}' || c == '#') {
           TextRange range = ExpressionUtils.findStringLiteralRange(expression, pos, pos + 1);
           holder.registerProblem(expression, range,
@@ -192,18 +207,13 @@ public class IncorrectDateTimeFormatInspection extends AbstractBaseJavaLocalInsp
         }
       }
 
-      private void checkPadding(@NotNull PsiExpression expression, char[] array, int pos) {
-        char c = array[pos];
-        if (c == 'p') {
-          int posP = pos;
-          while (posP + 1 < array.length && array[posP + 1] == 'p') {
-            posP++;
-          }
-          int afterP = posP + 1;
-          if (afterP >= array.length || !isLetter(array[afterP])) {
-            TextRange range = ExpressionUtils.findStringLiteralRange(expression, pos, afterP);
+      private void checkPadding(@NotNull PsiExpression expression, char[] array, int firstLetterIndex, int nextAfterP) {
+        if (array[firstLetterIndex] == 'p') {
+          if (nextAfterP >= array.length || !isLetter(array[nextAfterP])) {
+            TextRange range = ExpressionUtils.findStringLiteralRange(expression, firstLetterIndex, nextAfterP);
             holder.registerProblem(expression, range,
-                                   InspectionGadgetsBundle.message("inspection.incorrect.date.format.message.padding", c),
+                                   InspectionGadgetsBundle.message("inspection.incorrect.date.format.message.padding",
+                                                                   array[firstLetterIndex]),
                                    LocalQuickFix.EMPTY_ARRAY);
           }
         }
